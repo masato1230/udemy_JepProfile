d0a7599266cacdf47dd2df0cb76c2ee599e02229
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-08-13 15:56:00 +0900

    4_add_image_profile

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -3,19 +3,18 @@ package com.example.jetprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetprofile.ui.theme.JetProfileTheme
@@ -30,7 +29,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Column(
+                        modifier = Modifier.padding(20.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        // プロフィール画像
+                        Image(
+                            painter = painterResource(id = R.drawable.img_profile),
+                            contentDescription = "プロフィール",
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(10.dp)),
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        // 名前
                         Text(
                             text = "石川 正人",
                             color = Color.Gray,
@@ -38,6 +50,7 @@ class MainActivity : ComponentActivity() {
                             fontWeight = FontWeight.ExtraBold,
                         )
                         Spacer(modifier = Modifier.height(20.dp))
+                        // 職業
                         Text(text = "職業: Androidアプリ開発者", color = Color.Gray)
                     }
                 }
